73d1832fd93965939d0a920000b116305e3aa349
José Sánchez-Gallego <gallegoj@uw.edu>
2017-10-15 23:43:34 -0700

    adapted imports to PY3

--- python/lcoqa/__init__.py
+++ python/lcoqa/__init__.py
@@ -1,5 +1,5 @@
-from gcam import *
-from exposures import *
-from signal import *
-import plot
-import html
+from .gcam import *
+from .exposures import *
+from .signal import *
+import lcoqa.plot as plot
+import lcoqa.html as html
